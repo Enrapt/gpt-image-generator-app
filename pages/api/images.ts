3e938b11c9a2364bb4ea0f93639ac6e3a6e1a45f
hara <hara@enrapt.jp>
2023-04-12 17:51:57 +0900

    generate keyword input image

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -1,24 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai'
 
-type Image = {
-  url: string;
-};
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
 
-type ImagesApiResponse = {
-  images: Image[];
-};
+const openai = new OpenAIApi(configuration)
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ImagesApiResponse>
+  res: NextApiResponse
 ) {
   const { variables } = req.body;
+
+  const promptArray = variables.map((variable: { value: string; }) => variable.value);
+  const promptText = promptArray.join(",");
+
+  console.log(promptText);
+
   // 画像生成AIを使って画像を生成する処理を実装します（モックデータを返す）
-  const images = [
-    { url: "https://picsum.photos/id/237/200/300" },
-    { url: "https://picsum.photos/200/300?grayscale" },
-    { url: "https://picsum.photos/200/300" },
-  ];
+
+  const response = await openai.createImage({
+    prompt: promptText,
+    n: 4,
+    size: "512x512",
+    response_format: "url"
+  });
+
+  const images = await response.data.data
 
   res.status(200).json({ images });
 }
